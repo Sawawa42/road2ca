bedc802472591c888dab36baf953754292f840f3
syamasaw <seitaro.yamasawa@3-shake.com>
2025-07-05 17:53:56 +0900

    各初期化処理をmainに移動

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"flag"
 
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"road2ca/pkg/http/middleware"
 	"road2ca/pkg/server"
+	"road2ca/pkg/server/handler"
 )
 
 var (
@@ -17,5 +22,32 @@ func init() {
 }
 
 func main() {
-	server.Serve(addr)
+	// データベース接続の初期化
+	db := connectDB()
+	defer db.Close()
+
+	// ハンドラの初期化
+	h := handler.New(db)
+
+	// ミドルウェアの初期化
+	m := middleware.NewMiddleware(db)
+
+	server.Serve(addr, h, m)
+}
+
+// connectDB MySQLデータベースに接続する
+func connectDB() *sql.DB {
+	// TODO: .envからDSNを取得するようにする(正直今回は簡単のためハードコーディングでもいい気がする)
+	dsn := "root:ca-tech-dojo@tcp(localhost:3306)/road2ca?parseTime=true"
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %+v", err)
+	}
+
+	// DB接続の確認
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %+v", err)
+	}
+	return db
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"database/sql"
 	"log"
 	"road2ca/pkg/http/middleware"
 	"road2ca/pkg/minigin"
@@ -11,31 +10,12 @@ import (
 )
 
 // Serve HTTPサーバを起動する
-func Serve(addr string) {
-	// TODO: このあたりをまとめる
-	db, err := sql.Open("mysql", "root:ca-tech-dojo@tcp(localhost:3306)/road2ca?parseTime=true")
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %+v", err)
-	}
-	defer db.Close()
-
-	// DB接続の確認
-	if err := db.Ping(); err != nil {
-		log.Fatalf("Failed to ping database: %+v", err)
-	}
-	// このあたりここまで
-
-	// ハンドラの初期化
-	h := handler.New(db)
-
-	// ミドルウェアの初期化
-	m := middleware.NewMiddleware(db)
-
+func Serve(addr string, h *handler.Handler, m *middleware.Middleware) {
 	// ルーターの初期化
 	router := minigin.New()
 
 	// CORS対応など共通の設定を適用
-	router.Use(middleware.CommonConfig())
+	router.Use(m.CommonConfig)
 
 	/* ===== URLマッピングを行う ===== */
 	router.GET("/setting/get", h.HandleSettingGet)
@@ -44,20 +24,16 @@ func Serve(addr string) {
 
 	userGroup := router.Group("/user")
 	{
-		userGroup.Use(m.Authenticate) // 認証ミドルウェアを適用
+		// 認証ミドルウェアを適用
+		userGroup.Use(m.Authenticate)
+
 		userGroup.GET("/get", h.HandleUserGet)
 		// userGroup.POST("/update", h.HandleUserUpdate) // ex04
 	}
 
-	// TODO: 認証を行うmiddlewareを実装する
-	// middlewareは pkg/http/middleware パッケージを利用する
-	// http.HandleFunc("/user/get",
-	//   get(middleware.Authenticate(handler.HandleUserGet())))
-
 	/* ===== サーバの起動 ===== */
 	log.Println("Server running...")
-	err = router.Run(addr)
-	if err != nil {
+	if err := router.Run(addr); err != nil {
 		log.Fatalf("Server failed to start on %s: %+v", addr, err)
 	}
 }
