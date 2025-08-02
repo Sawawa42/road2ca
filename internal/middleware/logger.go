4a1c08d1c016cdb46816fac0788dfc4181d08413
syamasaw <seitaro.yamasawa@3-shake.com>
2025-08-02 17:49:52 +0900

    権限の修正

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -25,11 +25,11 @@ type logger struct {
 }
 
 func NewLogger() (Logger, error) {
-	accessFile, err := os.OpenFile("access.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	accessFile, err := os.OpenFile("access.log", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0644)
 	if err != nil {
 		return nil, err
 	}
-	errorFile, err := os.OpenFile("error.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	errorFile, err := os.OpenFile("error.log", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0644)
 	if err != nil {
 		return nil, err
 	}
